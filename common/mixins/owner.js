982e4405ae8e86c3e9f9c70d50286fa2b89d3a9c
Shing Lyu <shing.lyu@gmail.com>
2015-08-11 10:29:56 +0800

    Auto owner on mongo

--- common/mixins/owner.js
+++ common/mixins/owner.js
@@ -1,16 +1,10 @@
 var loopback = require('loopback');
 
 module.exports = function(Model, options) {
-  Model.defineProperty('creatorId', {type: "number", default: undefined});
   Model.observe('before save', function(ctx, next){
-    //var currentUser = ctx && ctx.get('currentUser');
-    //var currentUser=ctx.user
-      // Get the current access token
-    //   var accessToken = ctx.get('accessToken');
     var currentUser = loopback.getCurrentContext().get('currentUser')
     if (currentUser) {
-      //ctx.instance['creatorId'] = currentUser.id;
-      ctx.instance['creatorId'] = currentUser.id
+      ctx.instance['ownerId'] = currentUser.id
     } else {
       throw "Geee! We have a loophole in the ACL. Non-authorized user show not be able to create this Model";
     }
